5a26d4dd1f79f3b8df205198de099d2d0976a66b
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-06-03 14:13:48 -0600

    JS progress

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const form = document.querySelector('form');
 const card_1 = document.querySelector('.main-container');
 const card_2 = document.querySelector('.sub-container');
+const userEmail = document.querySelector('.email');
+const emailDisplay = document.querySelector('.user-email')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     card_1.classList.add('hide');
     card_2.classList.remove('hide');
+
+    valueEmail = userEmail.value;
+    
+    emailDisplay.innerHTML=valueEmail;
 });
+
 
 
