fd2f9c6076269d157da0eba068a9a6ba702bf586
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-06-03 16:24:37 -0600

    js validation progress

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const form = document.querySelector('form');
 const card_1 = document.querySelector('.main-container');
 const card_2 = document.querySelector('.sub-container');
 const userEmail = document.querySelector('.email');
-const emailDisplay = document.querySelector('.user-email')
+const emailDisplay = document.querySelector('.user-email');
+const error = document.querySelector('.error');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -12,6 +13,15 @@ form.addEventListener('submit', (e) => {
     valueEmail = userEmail.value;
     
     emailDisplay.innerHTML=valueEmail;
+    
+
+    const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if(valueEmail===""){
+        return console.log('Input Empty');
+    }
+
+    
 });
 
 
