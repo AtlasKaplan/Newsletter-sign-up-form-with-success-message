df11b3bdadd20814fb1fae975c1650fdd93d33f3
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-06-05 22:58:26 -0600

    proyect completed

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const form = document.querySelector('form');
 const card_1 = document.querySelector('.main-container');
 const card_2 = document.querySelector('.sub-container');
 const userEmail = document.querySelector('.email');
 const emailDisplay = document.querySelector('.user-email');
 const error = document.querySelector('.error');
 
+const form = document.querySelector('.form');
+const emailInput = document.querySelector('.email');
+const errorSpan = document.querySelector('.error');
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    card_1.classList.add('hide');
-    card_2.classList.remove('hide');
 
-    valueEmail = userEmail.value;
-    
-    emailDisplay.innerHTML=valueEmail;
-    
+    const email = emailInput.value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-    if(valueEmail===""){
-        return console.log('Input Empty');
+    if (!emailRegex.test(email)) {
+        errorSpan.classList.remove('hide');
+        userEmail.classList.add('error-box');
+    } else {
+        errorSpan.classList.add('hide');
+        card_1.classList.add('hide');
+        card_2.classList.remove('hide');
+        emailDisplay.textContent = email;
     }
-
-    
 });
-
-
-
